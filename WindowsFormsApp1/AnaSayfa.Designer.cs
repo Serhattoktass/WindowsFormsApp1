5c29e41ed05d62348f5f92282c2e9b160507d537
serha <serha@SERHATTOKTAS>
2022-06-02 15:10:13 +0300

    Proje dosyası ekle.

---
+++ WindowsFormsApp1/AnaSayfa.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class AnaSayfa
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AnaSayfa));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.ForeColor = System.Drawing.Color.Red;
+            this.panel1.Location = new System.Drawing.Point(12, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(971, 191);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(267, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(425, 36);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "PRESTIJ     RENT     A     CAR";
+            // 
+            // button6
+            // 
+            this.button6.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button6.ImageIndex = 5;
+            this.button6.ImageList = this.ımageList1;
+            this.button6.Location = new System.Drawing.Point(652, 68);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(130, 90);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Ana Sayfa";
+            this.button6.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // ımageList1
+            // 
+            this.ımageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList1.ImageStream")));
+            this.ımageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.ımageList1.Images.SetKeyName(0, "download.jpg");
+            this.ımageList1.Images.SetKeyName(1, "images.jpg");
+            this.ımageList1.Images.SetKeyName(2, "uyegiris.png");
+            this.ımageList1.Images.SetKeyName(3, "user.png");
+            this.ımageList1.Images.SetKeyName(4, "download.jpg");
+            this.ımageList1.Images.SetKeyName(5, "anaSf.jpg");
+            // 
+            // button5
+            // 
+            this.button5.ImageIndex = 4;
+            this.button5.ImageList = this.ımageList1;
+            this.button5.Location = new System.Drawing.Point(812, 67);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(136, 91);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Çıkış";
+            this.button5.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button4.ImageIndex = 3;
+            this.button4.ImageList = this.ımageList1;
+            this.button4.Location = new System.Drawing.Point(499, 66);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(130, 91);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Yeni Üyelik";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button3.ImageIndex = 2;
+            this.button3.ImageList = this.ımageList1;
+            this.button3.Location = new System.Drawing.Point(342, 66);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(130, 91);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Üye Girişi";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button2.ImageIndex = 0;
+            this.button2.ImageList = this.ımageList1;
+            this.button2.Location = new System.Drawing.Point(168, 66);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 91);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Şube Girişi";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.ImageIndex = 1;
+            this.button1.ImageList = this.ımageList1;
+            this.button1.Location = new System.Drawing.Point(13, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 91);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Yönetici Girişi";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // AnaSayfa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.aa;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(995, 475);
+            this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
+            this.Name = "AnaSayfa";
+            this.Text = "ANA SAYFA";
+            this.Load += new System.EventHandler(this.AnaSayfa_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ImageList ımageList1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
